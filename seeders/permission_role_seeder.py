7c14ff3c22ed83eb1426eedf8b87681cb9ea0f0d
miltonb <mj.beltran37@uniandes.edu.co>
2024-04-11 19:13:34 -0500

    Refactor SonarQube workflow and add new permissions

--- seeders/permission_role_seeder.py
+++ seeders/permission_role_seeder.py
@@ -22,7 +22,10 @@ def permission_roles_seed(db: Session) -> None:
             PermissionEnum.CREATE_EVENT,
             PermissionEnum.READ_EVENT,
             PermissionEnum.UPDATE_EVENT,
-            PermissionEnum.DEACTIVATE_EVENT
+            PermissionEnum.DEACTIVATE_EVENT,
+            PermissionEnum.CREATE_NUTRITIONAL_INFORMATION,
+            PermissionEnum.READ_ALLERGY_SPORTMAN,
+            PermissionEnum.MANAGE_ALLERGY
         ],
         RoleEnum.TERCERO.value: [
             PermissionEnum.READ_USER,
@@ -46,7 +49,10 @@ def permission_roles_seed(db: Session) -> None:
             PermissionEnum.CREATE_SERVICE,
             PermissionEnum.READ_SERVICE,
             PermissionEnum.READ_PRODUCT,
-            PermissionEnum.READ_EVENT
+            PermissionEnum.READ_EVENT,
+            PermissionEnum.CREATE_NUTRITIONAL_INFORMATION,
+            PermissionEnum.READ_ALLERGY_SPORTMAN,
+            PermissionEnum.MANAGE_ALLERGY
         ]
     }
     assign_permission_to_roles(db, roles_and_permissions)
